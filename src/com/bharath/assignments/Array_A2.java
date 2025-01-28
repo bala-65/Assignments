b89571b7a2ca558f1e55897f781343aedc1a0ff5
balajiravichandran <balajiravichandran26@gmail.com>
2025-01-28 21:26:52 +0530

    datatypes

---
+++ src/com/bharath/assignments/Array_A2.java
@@ -0,0 +1,9 @@
+package com.bharath.assignments;
+
+public class Array_A2 {
+	
+	public static void main (String args []) {
+	
+	
+}
+}
--- src/com/bharath/assignments/Datatypes_A1.java
+++ src/com/bharath/assignments/Datatypes_A1.java
@@ -4,7 +4,89 @@ public class Datatypes_A1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+     //temperature of a city in celsius
+		float cityTemperature= 25.5f;
+		System.out.println(cityTemperature);
+	
+	//order placement
+		boolean orderID= true;
+		System.out.println(orderID);
+		
+	//person's phone number
+		long phoneNumber= 9568998720l;
+		System.out.println(phoneNumber);
+		
+	//amount of money in customer's bank account
+		double moneyInCusAcc= 1000.50d;
+		System.out.println(moneyInCusAcc);
+		
+	//email address
+		String emailAddress= "johndurairaj21@gmail.com";
+		System.out.println(emailAddress);
+		
+	//coordinates of a location
+		float latitude= 37.7749f;
+		System.out.println(latitude);
+		float longitude= -122.4194f;
+		System.out.println(longitude);
+		
+	//person's marital status
+		boolean single= true;
+		System.out.println(single);
+		
+	//person's occupation
+		String occupation="Software Engineer";
+		System.out.println(occupation);
+		
+	//person's favourite color
+		String favouriteColor="Black";
+		System.out.println(favouriteColor);
+		
+	//current year
+		int currentYear= 2023;
+		System.out.println(currentYear);
+		
+	//number of followers on a social media
+		long noOfFollowers= 1000000;
+		System.out.println(noOfFollowers);
+		
+	//rating of a movie
+		float rating= 7.5f;
+		System.out.println(rating);
+	
+	//person's blood type
+		String bloodType="O+ve";
+		System.out.println(bloodType);
+		
+	//title of a book
+		String bookTitle= "The body keeps the score";
+		System.out.println(bookTitle);
+		
+	//number of employees in the company
+		int noOfEmployees=70000;
+		System.out.println(noOfEmployees);
+		
+	//time of an event
+	   String time= "2:30PM";
+	   System.out.println(time);
+	   
+	//name of a country
+	   String countryName="Netherlands";
+	   System.out.println(countryName);
+	   
+	//person's eye color
+	   String eyeColor="Blue";
+	   System.out.println(eyeColor);
+	   
+	//person's birthplace
+	   String birthPlace="amsterdam";
+	   System.out.println(birthPlace);
+	   
+	//distance between two cities
+	   double cityDistance= 198.75d;
+	   System.out.println(cityDistance);
 	}
+	
 
 }
