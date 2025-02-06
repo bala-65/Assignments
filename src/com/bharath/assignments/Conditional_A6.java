52e7c7b8d0aa9942887a14786d1dd2ae38a04226
balajiravichandran <balajiravichandran26@gmail.com>
2025-02-06 18:59:37 +0530

    assignment 6

--- src/com/bharath/assignments/Conditional_A6.java
+++ src/com/bharath/assignments/Conditional_A6.java
@@ -1,35 +1,40 @@
 package com.bharath.assignments;
 
 public class Conditional_A6 {
-	
-	public static void main (String [] args){
-		
-		//customer's details
-		String customerName="John doe";
-		int creditScore= 620;
-		float income= 55000.0f;
-		boolean isEmployed= true;
-		float debtToIncomeRatio= 35.0f;
-		
-		
-		//credit score greater than 750
-		if(creditScore>=750){
-			System.out.println("The customer's loan is automatically approved");
-			} 
-		
-		//credit score in between 650 and 750, income>=50000.0 && isEmployed== true && debtToIncomeRatio <= 40.0
-		else if(creditScore>=650 && creditScore<750){
-				System.out.println("Additional checks are performed for " + customerName);
-				if(income>=50000.0 && isEmployed== true && debtToIncomeRatio <= 40.0){
-					System.out.println("The loan is approved for " + customerName);}		
-		else
-			System.out.println("The loan is denied for " + customerName);
-		}
-			else{
-					System.out.println("The loan is denied for " + customerName);
-				}
-		 
-	}
-	
 
+	public static void main(String[] args) {
+
+		// customer's details
+		String customerName = "John doe";
+		int creditScore = 720;
+		float income = 55000.0f;
+		boolean isEmployed = true;
+		float debtToIncomeRatio = 35.0f;
+
+		// credit score greater than 750
+		if (creditScore >= 750) {
+			System.out.println("The customer's loan is automatically approved");
+		}
+
+		// credit score in between 650 and 750, income>=50000.0 && isEmployed== true
+		// debtToIncomeRatio <= 40.0
+		else if (creditScore >= 650 && creditScore < 750) {
+			System.out.println("Additional checks are performed for " + customerName +"!");
+			if (income >= 50000.0) {
+				if (isEmployed) {
+					if (debtToIncomeRatio < 40.0) {
+						System.out.println(customerName + " is eligible for the loan.");
+					} else {
+						System.out.println(customerName + " is not eligible for the loan because of the high DTI ratio.");
+					}
+				} else {
+					System.out.println(customerName + " is not eligible for the loan because of unemployment.");
+				}
+			} else {
+				System.out.println(customerName + " is not eligible for the loan as his income is below 50000.");
+			}
+		} else {
+			System.out.println(customerName + " is not eligible for the loan because of his poor credit score.");
+		}
+	}
 }
---
+++ src/com/bharath/assignments/Conditional_A7.java
@@ -0,0 +1,19 @@
+package com.bharath.assignments;
+
+public class Conditional_A7 {
+
+	public static void main(String[] args) {
+		int [] transactions= {-15000,-3000,-2000,-300,-200,3000,4000,50000};
+		
+		int totalCredits=0;
+		int totalDebits=0;
+		int totalAmountCredited=0;
+		int totalAmountDebited=0;
+		int remainingAmount=0;
+		int suspiciousTransactions=0;
+		
+		
+
+	}
+
+}
